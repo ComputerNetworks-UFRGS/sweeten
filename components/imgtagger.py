27ff1b36e6ec7aaa9cd81f9c18b9c8f0a6917f19
rnadal <rjmartins@inf.ufrgs.br>
2021-01-07 17:04:05 -0300

    Refactored imgtagger a bit. Added a function to try and pull a description from google (since default is often missing)

--- components/imgtagger.py
+++ components/imgtagger.py
@@ -5,22 +5,38 @@
 # Requires nltk for text processing and tag generation (TODO)
 
 import docker
+import requests
+import bs4
 
-#imageName = "sflow/prometheus"
-#imageName = "alpine"
+def enrichedDescriptionFromGoogle(imageName):
+        queryText = imageName
+        url = "https://google.com/search?q=define+" + queryText
+        requestResult = requests.get(url)
+        enrichedDescription = bs4.BeautifulSoup(requestResult.text,"html.parser").find("div", class_='BNeawe').text
+        return enrichedDescription
+
+def pullInfoFromDocker(imageName):
+        client = docker.from_env()
+        client.images.pull(imageName)
+        dockerImage = client.images.get(imageName)
+        imageTagsReadDict = dockerImage.tags
+        try:
+                imageDescription = dockerImage.labels["description"]
+                #print("Image default description: " + imageDescription)
+        except:
+                imageDescription = ""
+                #print("No default image description available")
+        return dockerImage, imageTagsReadDict, imageDescription
+
 imageName = "eclipse-mosquitto"
+#imageName = "sflow/prometheus"
 
-client = docker.from_env()
+dockerImage, imageDefaultTags, imageDefaultDescription = pullInfoFromDocker(imageName)
+enrichedDescription = enrichedDescriptionFromGoogle(imageName)
 
-dockerImage = client.images.get(imageName)
-
-imageTagsReadDict = dockerImage.tags
-
-print(dockerImage)
-print(imageTagsReadDict)
-try:
-        imageDescription = dockerImage.labels["description"]
-        print(imageDescription)
-except:
-        imageDescription = ""
-        print("No description available")
+print("Docker image name: " + imageName)
+print("Image original tags: ", end="")
+for tag in imageDefaultTags:
+        print(tag)
+print("Image original description: " + imageDefaultDescription)
+print("Enriched description: " + enrichedDescription)
